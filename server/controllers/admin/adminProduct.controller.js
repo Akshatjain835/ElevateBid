4ad76ff8a83a89509db3919149a56bdc0feefd1d
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-14 22:55:14 +0530

    feat: added delete product by admin controller in admin product controller

--- server/controllers/admin/adminProduct.controller.js
+++ server/controllers/admin/adminProduct.controller.js
@@ -27,3 +27,29 @@ export const verifyAndAddCommissionProductByAmdinController = async (req, res) =
          data: product
          });
   };
+
+export const getAllProductsByAmdinController = async (req, res) => {
+    const products = await Product.find({}).sort("-createdAt").populate("user");
+  
+    // console.log(products);
+
+  };
+
+export const deleteProductsByAmdinController = async (req, res) => {
+    try {
+      const { productIds } = req.body;
+        // console.log(productIds);
+  
+      const result = await Product.findOneAndDelete({ _id: productIds });
+  
+      res.status(200).json({ 
+        message: `${result.deletedCount} products deleted successfully` 
+    });
+    } catch (error) {
+
+      res.status(500).json({ 
+        message: "Internal server error",
+        error: error.message 
+    });
+    }
+  };
--- server/routes/admin/adminProduct.route.js
+++ server/routes/admin/adminProduct.route.js
@@ -1,10 +1,12 @@
 import Router from "express";
 
 import { isAdmin, protectMiddleware } from "../../middlewares/authMiddleware.js";
-import { verifyAndAddCommissionProductByAmdinController } from "../../controllers/admin/adminProduct.controller.js";
+import { deleteProductsByAmdinController, getAllProductsByAmdinController, verifyAndAddCommissionProductByAmdinController } from "../../controllers/admin/adminProduct.controller.js";
 
 const adminProductRouter = Router();
 
 adminProductRouter.patch("/admin/product-verified/:id", protectMiddleware, isAdmin, verifyAndAddCommissionProductByAmdinController);
+adminProductRouter.get("/admin/products", protectMiddleware, isAdmin, getAllProductsByAmdinController);
+adminProductRouter.delete("/admin/products", protectMiddleware, isAdmin, deleteProductsByAmdinController);
 
 export default adminProductRouter
