b18e9a24f8b67de1db3eaeea47602646fdf18726
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-17 23:34:09 +0530

    Update: updated Header component

--- client/src/components/common/Design.jsx
+++ client/src/components/common/Design.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-
+import { NavLink } from "react-router-dom";
 const Container = ({ children, className }) => {
   return <div className={`${className} w-[85%] m-auto`}>{children}</div>;
 };
@@ -17,13 +17,31 @@ const CustomNavLinkList = ({ href, className, isActive, children }) => {
     );
   };
 
-export { Container,CustomNavLinkList };
+const CustomNavLink = ({ href, className, isActive, children }) => {
+    const linkStyles = "text-[17px] font-medium cursor-pointer list-none hover:text-green transition-all ease-in-out";
+    const activeClass = isActive ? "bg-green_100 text-green" : "";
+  
+    return (
+      <>
+        <NavLink to={href} className={`${className} ${linkStyles} ${activeClass}`}>
+          {children}
+        </NavLink>
+      </>
+    );
+  };
+export { Container,CustomNavLinkList,CustomNavLink };
 export const commonClassNameOfInput = "w-full p-4 text-sm text-gray-900 border border-gray-200 focus:ring-green focus:border-green outline-none";
 
 Container.propTypes = {
     children: PropTypes.any,
     className: PropTypes.any,
 };
+CustomNavLink.propTypes = {
+    href: PropTypes.any,
+    className: PropTypes.any,
+    children: PropTypes.any,
+    isActive: PropTypes.any,
+  };
 CustomNavLinkList.propTypes = {
     href: PropTypes.any,
     className: PropTypes.any,
--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,71 +1,121 @@
-import React from 'react'
-import { Container, CustomNavLinkList } from './Design';
+import React, { useEffect, useRef, useState } from 'react'
+import { Container, CustomNavLink, CustomNavLinkList } from './Design';
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { IoSearchOutline } from "react-icons/io5";
-
+import { useLocation } from 'react-router-dom';
+import { menulists } from '../../utils/data';
+import ProfileCard from '../cards/ProfileCard';
+const User1 = "https://cdn-icons-png.flaticon.com/128/6997/6997662.png";
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const location = useLocation();
+    // console.log(location)
+    const menuRef = useRef(null);
+    // console.log(menuRef)
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
+    
+  const closeMenuOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", closeMenuOutside);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("mousedown", closeMenuOutside);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Check if it's the home page
+  const isHomePage = location.pathname === "/";
+
+  const role = "buyer";
+
+
     return (
         <>
-          <header>
-            <Container>
+        <header className={isHomePage ? `header py-1 bg-primary ${isScrolled ? "scrolled" : ""}` : `header bg-white shadow-s1 ${isScrolled ? "scrolled" : ""}`}>
+          <Container>
+            <nav className="p-4 flex justify-between items-center relative">
+              <div className="flex items-center gap-14">
 
-              <nav className="p-4 flex justify-between items-center relative">
-                <div className="flex items-center gap-14">
-
-                  <div>
-                    {isHomePage && !isScrolled ? (
-                      <img src="../images/common/header-logo.png" alt="ElevateBid" className="h-11" />
-                    ) : (
-                      <img src="../images/common/header-logo2.png" alt="Elevate bid" className="h-11" />
-                    )}
-                  </div>
-
-                  <div className="hidden lg:flex items-center justify-between gap-8">
-                    {menulists.map((list) => (
-                      <li key={list.id} className="capitalize list-none">
-                        <CustomNavLinkList href={list.path} >
-                          {list.link}
-                        </CustomNavLinkList>
-                      </li>
-                    ))}
-                  </div>
-                  
+                <div>
+                  {isHomePage && !isScrolled ? (
+                    <img src="../images/common/header-logo.png" alt="LogoImg" className="h-11" />
+                  ) : (
+                    <img src="../images/common/header-logo2.png" alt="LogoImg" className="h-11" />
+                  )}
                 </div>
 
-                <div className="flex items-center gap-8 icons">
-
-                  <div className="hidden lg:flex lg:items-center lg:gap-8">
-                    <IoSearchOutline size={23}  />
-                    {role === "buyer" && (
-                      <CustomNavLink href="/seller/login" >
-                        Become a Seller
-                      </CustomNavLink>
-                    )}
-
-                    <CustomNavLink href="/login">
-                      Sign in
-                    </CustomNavLink>
-
-                    <CustomNavLink href="/register" >
-                      Join
-                    </CustomNavLink>
-
-                    <CustomNavLink href="/dashboard">
-                      <ProfileCard>
-                        <img src={User1} alt="" className="w-full h-full object-cover" />
-                      </ProfileCard>
-                    </CustomNavLink>
-
-                  </div>
-
-                
+                <div className="hidden lg:flex items-center justify-between gap-8">
+                  {menulists.map((list) => (
+                    <li key={list.id} className="capitalize list-none">
+                      <CustomNavLinkList href={list.path} isActive={location.pathname === list.path} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>
+                        {list.link}
+                      </CustomNavLinkList>
+                    </li>
+                  ))}
                 </div>
-    
-                
-              </nav>
-            </Container>
-          </header>
-        </>
+
+              </div>
+
+              <div className="flex items-center gap-8 icons">
+
+                <div className="hidden lg:flex lg:items-center lg:gap-8">
+                  <IoSearchOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
+                  {role === "buyer" && (
+                    <CustomNavLink href="/seller/login" className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>
+                      Become a Seller
+                    </CustomNavLink>
+                  )}
+                  <CustomNavLink href="/login" className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>
+                    Sign in
+                  </CustomNavLink>
+                  <CustomNavLink href="/register" className={`${!isHomePage || isScrolled ? "bg-green" : "bg-white"} px-8 py-2 rounded-full text-primary shadow-md`}>
+                    Join
+                  </CustomNavLink>
+                  <CustomNavLink href="/dashboard">
+                    <ProfileCard>
+                      <img src={User1} alt="" className="w-full h-full object-cover" />
+                    </ProfileCard>
+                  </CustomNavLink>
+                </div>
+
+                <div className={`icon flex items-center justify-center gap-6 ${isScrolled || !isHomePage ? "text-primary" : "text-white"}`}>
+                  <button onClick={toggleMenu} className="lg:hidden w-10 h-10 flex justify-center items-center bg-black text-white focus:outline-none">
+                    {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+                  </button>
+                </div>
+
+              </div>
+  
+              {/* Responsive Menu if below 768px */}
+              <div ref={menuRef} className={`lg:flex lg:items-center lg:w-auto w-full p-5 absolute right-0 top-full menu-container ${isOpen ? "open" : "closed"}`}>
+                {menulists.map((list) => (
+                  <li href={list.path} key={list.id} className="uppercase list-none">
+                    <CustomNavLink className="text-white">{list.link}</CustomNavLink>
+                  </li>
+                ))}
+              </div>
+              
+            </nav>
+          </Container>
+        </header>
+      </>
       );
 }
 
