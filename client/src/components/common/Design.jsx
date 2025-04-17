f59f791cbc7237ec448894ddcafc9a6768f553bb
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-17 22:59:53 +0530

    feat: added Header component in common component folder

--- client/src/components/common/Design.jsx
+++ client/src/components/common/Design.jsx
@@ -4,10 +4,29 @@ const Container = ({ children, className }) => {
   return <div className={`${className} w-[85%] m-auto`}>{children}</div>;
 };
 
-export { Container };
+const CustomNavLinkList = ({ href, className, isActive, children }) => {
+    const linkStyles = "text-[17px] font-medium cursor-pointer list-none hover:text-green transition-all ease-in-out";
+    const activeClass = isActive ? "text-green" : "";
+  
+    return (
+      <>
+        <NavLink to={href} className={`${className} ${linkStyles} ${activeClass}`}>
+          {children}
+        </NavLink>
+      </>
+    );
+  };
+
+export { Container,CustomNavLinkList };
 export const commonClassNameOfInput = "w-full p-4 text-sm text-gray-900 border border-gray-200 focus:ring-green focus:border-green outline-none";
 
 Container.propTypes = {
     children: PropTypes.any,
     className: PropTypes.any,
 };
+CustomNavLinkList.propTypes = {
+    href: PropTypes.any,
+    className: PropTypes.any,
+    children: PropTypes.any,
+    isActive: PropTypes.any,
+  };
--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import { Container, CustomNavLinkList } from './Design';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+          <header>
+            <Container>
+
+              <nav className="p-4 flex justify-between items-center relative">
+                <div className="flex items-center gap-14">
+
+                  <div>
+                    {isHomePage && !isScrolled ? (
+                      <img src="../images/common/header-logo.png" alt="ElevateBid" className="h-11" />
+                    ) : (
+                      <img src="../images/common/header-logo2.png" alt="Elevate bid" className="h-11" />
+                    )}
+                  </div>
+
+                  <div className="hidden lg:flex items-center justify-between gap-8">
+                    {menulists.map((list) => (
+                      <li key={list.id} className="capitalize list-none">
+                        <CustomNavLinkList href={list.path} >
+                          {list.link}
+                        </CustomNavLinkList>
+                      </li>
+                    ))}
+                  </div>
+                  
+                </div>
+
+                <div className="flex items-center gap-8 icons">
+
+                  <div className="hidden lg:flex lg:items-center lg:gap-8">
+                    <IoSearchOutline size={23}  />
+                    {role === "buyer" && (
+                      <CustomNavLink href="/seller/login" >
+                        Become a Seller
+                      </CustomNavLink>
+                    )}
+
+                    <CustomNavLink href="/login">
+                      Sign in
+                    </CustomNavLink>
+
+                    <CustomNavLink href="/register" >
+                      Join
+                    </CustomNavLink>
+
+                    <CustomNavLink href="/dashboard">
+                      <ProfileCard>
+                        <img src={User1} alt="" className="w-full h-full object-cover" />
+                      </ProfileCard>
+                    </CustomNavLink>
+
+                  </div>
+
+                
+                </div>
+    
+                
+              </nav>
+            </Container>
+          </header>
+        </>
+      );
 }
 
 export default Header
