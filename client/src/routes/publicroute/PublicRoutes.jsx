01b3afd8bceb0fdaec6fc5d6cac4e54bb78fb50f
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-19 01:53:20 +0530

    feat: added ProductDetails Page

--- client/src/routes/publicroute/PublicRoutes.jsx
+++ client/src/routes/publicroute/PublicRoutes.jsx
@@ -1,11 +1,24 @@
 import { Layout } from "../../components/common/layout/Layout.jsx";
 import Home from "../../pages/home/Home.jsx";
+import { ProductsDetailsPage } from "../../pages/product/ProductDetailsPage.jsx";
 
 const PublicRoutes = [
-    {
-        path: "/",
-        element: <Layout><Home /></Layout>,
-      },
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: "/details/:id",
+    element: (
+      <Layout>
+        <ProductsDetailsPage />
+      </Layout>
+    ),
+  },
 ];
 
 export default PublicRoutes;
